410a208883c1af1e20fc0cc113f0a83bab1b0e02
andreabac3 <bacciu.1747105@studenti.uniroma1.it>
2019-07-01 02:13:16 +0200

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,15 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
-
+function maxChar(str) {
+	let dict = {};
+  for (let i = 0; i < str.length ; i++){
+  	if (dict[str[i]] == undefined){
+  		dict[str[i]] = 0;
+  	}
+  	dict[str[i]] += 1;
+  }
+  return Object.keys(dict).reduce((a, b) => dict[a] > dict[b] ? a : b);
+}
+console.log(maxChar("abcccccccd"));
 module.exports = maxChar;
