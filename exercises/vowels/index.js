e82d9e9ac6ca01daac36a6768179f4c650a1991e
andreabac3 <bacciu.1747105@studenti.uniroma1.it>
2019-07-02 16:18:15 +0200

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,7 +8,7 @@
 //   vowels('Why?') --> 0
 
 
-// NB many overheads, a solution to practice with the Reduce function
+// N.B. many overheads, a solution to practice with the Reduce function
 
 function countVolwels(cnt, char){
   const volwels = "aeiou";
