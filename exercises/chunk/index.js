ce63a151a5ba79f2c02cd10a69c34b47d28df802
andreabac3 <bacciu.1747105@studenti.uniroma1.it>
2019-07-02 02:09:31 +0200

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,38 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
 
+
+// SOL without slice function
+function chunk2(array, size) {
+	let result = [[]];
+	let n = 0;
+	let iter = 0;
+	while (n < array.length){
+		for(let i = 0 ; i < size ; i++){
+			if (n > array.length-1){
+				return result;
+			}
+			result[iter].push(array[n]);
+			n++;
+		}
+		iter++;
+		result.push(new Array());
+	}
+	return result;
+}
+
+
+// sol with slice
+function chunk(array, size) {
+	let result = [];
+	let n = 0;
+	while (n < array.length){
+		result.push(array.slice(n, n + size));
+		n += size;
+	}
+	return result;
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
 module.exports = chunk;
